a8c4105b59d6e32c0d18fb2ac023719cd8b80da6
MohamedAhmed <mohamed.essmattawfik@gmail.com>
2022-11-03 04:07:44 +0200

    add production endpoint for api

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,5 +5,5 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0',
+  apiHost: 'http://udagram-api-final.eba-njgpvptv.us-east-1.elasticbeanstalk.com//api/v0',
 };
